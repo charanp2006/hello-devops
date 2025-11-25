1e6e87ba9f45345c20604f0d8802b236726361e8
charanp2006 <charan.p.8653@gmail.com>
2025-11-25 10:36:25 +0530

    4 lines added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  // res.send("Hello DevOps CI/CD Pipeline!");
-  res.send("Project Deployed Automatically!");
+  res.send("Hello DevOps CI/CD Pipeline!");
+  // res.send("Project Deployed Automatically!");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
