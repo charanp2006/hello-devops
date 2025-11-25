bf4d00d2009d2ecc3b8288c4fd6f569f03061e30
charanp2006 <charan.p.8653@gmail.com>
2025-11-25 08:27:26 +0530

    Testing version update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps CI/CD Pipeline!");
+  // res.send("Hello DevOps CI/CD Pipeline!");
   res.send("Testing Version 2 Deployed Automatically!");
 });
 
