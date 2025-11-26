5bfd494bddbd433a74171af33d7119e0ce2a9a77
charanp2006 <charan.p.8653@gmail.com>
2025-11-26 17:04:56 +0530

    Project Completed

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  // res.send("OEE");
-  res.send("Project deployed Automatically ! Testing ");
+  res.send("Project deployed Automatically!");
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
